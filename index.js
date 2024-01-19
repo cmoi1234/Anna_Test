dfc815942619a6ed0aa8a58615166b595f75c0e0
quentin <qdimand@gmail.com>
2024-01-19 15:38:18 -0300

    modiff par bob

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function showtime(){
+function showTime(){
     var date = new date();
     var h = date.getHours();
     var m = date.getMinutes();
     var s = date.getSeconds();
 
-    h = (h<10) ? "0" + h : h;
-    m = (m<10) ? "0" + m : m;
-    s = (s<10) ? "0" + s : s;
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
 
-    var time = h + ":" + m + ":" + s + " " + session;
+    var time = h + ":" + m + ":" + s ;
     document.getElementsById("DigitalCLOCK").innerText = time;
     document.getElementsById("DigitalCLOCK").textContent = time;
 
